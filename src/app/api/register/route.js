61ecf2a758e04b1377bf57176cf88cd88c958bd2
milliondreamsblog <akshatsan23@gmail.com>
2026-02-02 12:55:11 +0530

    update

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -23,6 +23,6 @@ export async function POST(req) {
 
   } catch (error) {
     console.error("DB Error:", error);
-    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+    return NextResponse.json({ message: "Internal Server Err" }, { status: 500 });
   }
 }
